05bcbcc165a1a9bcc544abd1ba5a9a2501a544e4
Rayanne Macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2025-09-03 09:26:49 -0300

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,12 +14,12 @@ module.exports = {
     sourceType: 'module',
   },
  rules: {
-  '@typescript-eslint/no-unused-vars': ['error', {
-    argsIgnorePattern: '^_',
-    varsIgnorePattern: '^_',
-    caughtErrorsIgnorePattern: '^_',
-  }],
-  '@typescript-eslint/no-explicit-any': 'warn',
+  // '@typescript-eslint/no-unused-vars': ['error', {
+  //   argsIgnorePattern: '^_',
+  //   varsIgnorePattern: '^_',
+  //   caughtErrorsIgnorePattern: '^_',
+  // }],
+  //'@typescript-eslint/no-explicit-any': 'warn',
   '@typescript-eslint/explicit-function-return-type': 'off',
   '@typescript-eslint/explicit-module-boundary-types': 'off',
   '@typescript-eslint/no-inferrable-types': 'off',
